1c64958d5d9e27a2470b8cdaac314a7884f18f2b
Parth_V <parthvashisht20@gmail.com>
2020-07-03 23:58:26 +0530

    finished initial setup

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    [SerializeField] Paddle paddle1;
+    [SerializeField] float xVel = 2f;
+    [SerializeField] float yVel = 15f;
+
+    bool hasLaunched = false;
+    Vector2 ballPosRelToPaddle;
+    // Start is called before the first frame update
+    void Start()
+    {
+        ballPosRelToPaddle = transform.position - paddle1.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!hasLaunched)
+        {
+            LockBallToPaddle();
+            LaunchOnMouseClick();
+        }
+    }
+
+    private void LaunchOnMouseClick()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            hasLaunched = true;
+            GetComponent<Rigidbody2D>().velocity = new Vector2(xVel, yVel);
+        }
+    }
+
+    private void LockBallToPaddle()
+    {
+        Vector2 paddlePos = new Vector2(paddle1.transform.position.x, paddle1.transform.position.y);
+        transform.position = paddlePos + ballPosRelToPaddle;
+    }
+}
