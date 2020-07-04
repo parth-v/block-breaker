00e0a5c7f32653503c203e6777aaaf25e7175839
Parth_V <parthvashisht20@gmail.com>
2020-07-04 16:38:29 +0530

    adding audio to level 2

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -5,16 +5,24 @@ using UnityEngine;
 
 public class Ball : MonoBehaviour
 {
+    //config params
     [SerializeField] Paddle paddle1;
     [SerializeField] float xVel = 2f;
     [SerializeField] float yVel = 15f;
+    [SerializeField] AudioClip[] ballSounds;
 
+    //state
     bool hasLaunched = false;
     Vector2 ballPosRelToPaddle;
+
+    //cached component references
+    AudioSource myAudioSource;
+
     // Start is called before the first frame update
     void Start()
     {
         ballPosRelToPaddle = transform.position - paddle1.transform.position;
+        myAudioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -40,5 +48,14 @@ public class Ball : MonoBehaviour
     {
         Vector2 paddlePos = new Vector2(paddle1.transform.position.x, paddle1.transform.position.y);
         transform.position = paddlePos + ballPosRelToPaddle;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (hasLaunched)
+        {
+            AudioClip clip = ballSounds[UnityEngine.Random.Range(0, ballSounds.Length)];
+            myAudioSource.PlayOneShot(clip);
+        }
     }
 }
