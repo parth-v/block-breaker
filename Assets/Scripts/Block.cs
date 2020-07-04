2825ae908f3f71bc0e5f82a562147bc930959e01
Parth_V <parthvashisht20@gmail.com>
2020-07-04 16:56:08 +0530

    added audio to whole game

--- Assets/Scripts/Block.cs
+++ Assets/Scripts/Block.cs
@@ -4,8 +4,11 @@ using UnityEngine;
 
 public class Block : MonoBehaviour
 {
+    [SerializeField] AudioClip blockSound;
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
+        AudioSource.PlayClipAtPoint(blockSound, Camera.main.transform.position);
         Destroy(gameObject);
     }
 }
