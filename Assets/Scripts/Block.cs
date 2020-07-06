bf93c974dc6dc70349cda4516f9eaa3d9bbc1ee7
Parth_V <parthvashisht20@gmail.com>
2020-07-06 21:15:22 +0530

    added win functionalities

--- Assets/Scripts/Block.cs
+++ Assets/Scripts/Block.cs
@@ -6,9 +6,18 @@ public class Block : MonoBehaviour
 {
     [SerializeField] AudioClip blockSound;
 
+    //cached reference
+    Level level;
+    private void Start()
+    {
+        level = FindObjectOfType<Level>();
+        level.CountBreakableBlocks();
+    }
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
         AudioSource.PlayClipAtPoint(blockSound, Camera.main.transform.position);
         Destroy(gameObject);
+        level.BlockDestroyed();
     }
 }
